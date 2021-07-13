6ef8f9364cccd3a553eb24edb61b2f181d9dce32
Willem Bressers <dhr.bressers@gmail.com>
2021-07-13 20:46:14 +0200

    added the derivative, PID controller works

--- src/PID.cpp
+++ src/PID.cpp
@@ -8,16 +8,19 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   // Initialize PID coefficients
   Kp = Kp_;
   Ki = Ki_;
-  // Kd = Kd_;
+  Kd = Kd_;
 
   // Initialize coeffiecent errors
-  // d_error = 0;
+  d_error = 0;
   i_error = 0;
   p_error = 0;
 }
 
 void PID::UpdateError(double cte) {
   // Update PID errors based on cte.
+
+  // calculate the difference between the previous and the current cross track error
+  d_error = cte - p_error;
 
   // set the most recent cross track error
   p_error = cte;
@@ -41,5 +44,9 @@ double PID::GetSteeringValue() {
   // calculate the intergral value
   double I = -Ki * i_error;
 
-  return P + I;
+  // calculate the derivative value
+  double D = -Kd * d_error;
+
+  // combine the P,I,D for the steering value
+  return P + I + D;
 }
